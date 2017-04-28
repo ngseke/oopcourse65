a90a89bf3c08dd21ea7f8301d2b9673ebd86c93c
sean <sean@SEAN3AE5>
2017-04-28 09:30:58 +0800

    astyled

--- game4.10/Source/CBossB.cpp
+++ game4.10/Source/CBossB.cpp
@@ -29,45 +29,45 @@ CBossB::CBossB(int x, int y, int delay, bool alive, CDict* d, int minVL, int max
     this->bombList = bombList;
     minVocabLeng = minVL;
     maxVocabLeng = maxVL;
-    callEnemyCounter = maxCallEnemyCounter =500;		// 發動召喚小怪技能的間隔
+    callEnemyCounter = maxCallEnemyCounter = 500;		// 發動召喚小怪技能的間隔
     endX = SIZE_X / 2;
     endY = SIZE_Y;
     //
     SetVocab();
 }
-CBossB::CBossB(int x, int y, int delay, bool alive, CDict* d, int minVL, int maxVL, vector<CEnemy*>* enemyQueue, vector<CBomb*>* bombList,int endx,int endy) {	//	初始值都在此處設定
-	this->enemyQueue = enemyQueue;
-	this->bombList = bombList;
-	is_alive = is_bombed = false;
-	dx = dy = index = delay_counter = 0;
-	currWordLeng = 0;
-	targetX = -2;
-	targetY = -2;
-	////
-	SetXY(x, y);
-	SetDelay(delay);
-	SetIsAlive(alive);
-	dict = d;
-	this->bombList = bombList;
-	minVocabLeng = minVL;
-	maxVocabLeng = maxVL;
-	callEnemyCounter = maxCallEnemyCounter = 1000;		// 發動召喚小怪技能的間隔
-	this->endX = endx;
-	this->endY = endy;
-	//
-	SetVocab();
+CBossB::CBossB(int x, int y, int delay, bool alive, CDict* d, int minVL, int maxVL, vector<CEnemy*>* enemyQueue, vector<CBomb*>* bombList, int endx, int endy) {	//	初始值都在此處設定
+    this->enemyQueue = enemyQueue;
+    this->bombList = bombList;
+    is_alive = is_bombed = false;
+    dx = dy = index = delay_counter = 0;
+    currWordLeng = 0;
+    targetX = -2;
+    targetY = -2;
+    ////
+    SetXY(x, y);
+    SetDelay(delay);
+    SetIsAlive(alive);
+    dict = d;
+    this->bombList = bombList;
+    minVocabLeng = minVL;
+    maxVocabLeng = maxVL;
+    callEnemyCounter = maxCallEnemyCounter = 1000;		// 發動召喚小怪技能的間隔
+    this->endX = endx;
+    this->endY = endy;
+    //
+    SetVocab();
 }
 void CBossB::CallEnemy(int x, int y) {
-	enemyQueue->push_back(new CEnemy(x + (bmp.Width()/2)-5, y+40, 3, 1, dict, 3, 4, bombList, x, int(y * 5  )));//正中間往下(90度)
+    enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2) - 5, y + 40, 3, 1, dict, 3, 4, bombList, x, int(y * 5  ))); //正中間往下(90度)
     enemyQueue->back()->LoadBitmap();
-	enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2)-5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(0.785))), int(y * 5 * (sin(0.785)))));//45度
-	enemyQueue->back()->LoadBitmap();
-	enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2)-5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(1.1775))), int(y * 5 * (sin(1.1775)))));//67.5度
-	enemyQueue->back()->LoadBitmap();
-	enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2)-5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(1.9625))), int(y * 5 * (sin(1.9625)))));//112.5度
-	enemyQueue->back()->LoadBitmap();
-	enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2)-5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(2.355))), int(y * 5 * (sin(2.355)))));//135度
-	enemyQueue->back()->LoadBitmap();
+    enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2) - 5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(0.785))), int(y * 5 * (sin(0.785))))); //45度
+    enemyQueue->back()->LoadBitmap();
+    enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2) - 5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(1.1775))), int(y * 5 * (sin(1.1775))))); //67.5度
+    enemyQueue->back()->LoadBitmap();
+    enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2) - 5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(1.9625))), int(y * 5 * (sin(1.9625))))); //112.5度
+    enemyQueue->back()->LoadBitmap();
+    enemyQueue->push_back(new CEnemy(x + (bmp.Width() / 2) - 5, y + 40, 3, 1, dict, 3, 4, bombList, int(x * 5 * (cos(2.355))), int(y * 5 * (sin(2.355))))); //135度
+    enemyQueue->back()->LoadBitmap();
 }
 void CBossB::OnMove() {
     const int STEPS = 300;	// 切成幾分dx
